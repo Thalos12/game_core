d58e1bdc4f0990d0b4bf602b2d557f7abf27eab0
Alessandro <mazzalex02@libero.it>
2015-12-03 18:47:23 +0100

    Minor update.

--- core/profiledb.py
+++ core/profiledb.py
@@ -3,8 +3,8 @@ import sqlite3 as sql
 root = os.path.dirname(os.path.realpath(__file__))
 
 def save(stats):
+    print "SAVING"
     con = sql.connect(os.path.join(root,'data','players',stats['NAME']+'.datafile'))
-
     with con:
         cur = con.cursor()
         if cur.execute("SELECT EXISTS(SELECT 1 FROM info WHERE name=? LIMIT 1)",stats['NAME']).fetchone()[0] == 1:
@@ -12,12 +12,30 @@ def save(stats):
             cur.execute("UPDATE info SET level=?, exp=?, money=? WHERE name=?",(stats['LEVEL'],stats['EXP'],stats['MONEY'],stats['NAME']))
             print "Profile updated."
         else:
-            print "Profile doesn't exist."
+            print "Profile does not exist."
             cur.execute("INSERT INTO info VALUES(?,?,?,?)",(stats['NAME'],stats['LEVEL'],stats['EXP'],stats['MONEY']))
             print 'Profile inserted.'
+    print '\n',
+
+def load(name):
+    print "LOADING"
+    con = sql.connect(os.path.join(root,'data','players',str(name)+'.datafile'))
+    with con:
+        cur = con.cursor()
+        if cur.execute("SELECT EXISTS(SELECT 1 FROM info WHERE name=? LIMIT 1)",str(name)).fetchone()[0] == 1:
+            print "Profile exists."
+        else:
+            print 'Profile does not exist.'
+            return None
+        stats = {'NAME':name}
+        stats['LEVEL'] = cur.execute("SELECT level FROM info WHERE name=?",str(name)).fetchone()[0]
+        stats['EXP'] = cur.execute("SELECT exp FROM info WHERE name=?",str(name)).fetchone()[0]
+        stats['MONEY'] = cur.execute("SELECT money FROM info WHERE name=?",str(name)).fetchone()[0]
+        print 'Profile loaded.'
+        print stats
+    print '\n',
+
 if __name__ == '__main__':
-    stats = {'NAME':'p',
-             'LEVEL':1,
-             'EXP':0,
-             'MONEY':15}
+    stats = {'NAME':'p', 'LEVEL':1, 'EXP':0, 'MONEY':25}
     save(stats)
+    load('p')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import sys
 import time
+import ConfigParser
 
 from core import battle_manager
 from core import update
@@ -13,48 +14,68 @@ from core.settings.menu_settings import *
 
 sys.path.append(os.getcwd())
 
+_version = 'pre-alpha1'
+
 # start logging
 logging.basicConfig(filename=os.path.join('core', 'logs', str(int(time.time())) + '.txt'), level=logging.DEBUG)
+
 logging.info("{}".format(time.strftime("Started logging %d %b %Y, %H:%M:%S")))
+logging.info("game_core version is {}".format(_version))
+
+# load basic configurations
+config = ConfigParser.ConfigParser()
+config.read('game_core.cfg')
+textual = config.get('MODE','textual')
+logging.info("Textual mode is set to {}.".format(textual))
+volume = config.get('AUDIO','volume')
+logging.info("Audio volume is set to {}.".format(volume))
 
 # noinspection PyPep8Naming
 class Game(object):
     def __init__(self):
-        print ("\nWelcome dear adventurer! If this is your first time playing, "
-               "then I welcome you and hope you will have a pleasant journey. "
-               "If you are already a player,then why are you still here reading "
-               "this? Get out there!\n")
-
-        name = raw_input('Insert your name, please: ')
+        if textual:
+            print ("\nWelcome dear adventurer! If this is your first time playing, "
+                   "then I welcome you and hope you will have a pleasant journey. "
+                   "If you are already a player,then why are you still here reading "
+                   "this? Get out there!\n")
+            name = raw_input('Insert your name, please: ')
+            
         # noinspection PyBroadException
         try:
             open(os.path.join(root, 'core', 'data', 'players', name + '.profile'))
-            print "\nWelcome back {}!\n".format(name)
+            if textual:
+                print "\nWelcome back {}!\n".format(name)
         except:
-            print "\nNo player named {} found.\n".format(name)
-            a = raw_input('Would you like to create a new profile?[y/n]').lower() or 'y'
+            if textual:
+                print "\nNo player named {} found.\n".format(name)
+                a = raw_input('Would you like to create a new profile?[y/n]').lower() or 'y'
+            else:
+                sys.exit()
             if a == 'y':
                 profile.create(name)
             elif a == 'n':
-                print "Have a good day."
+                if textual:
+                    print "Have a good day."
                 logging.info("{}".format(time.strftime("END LOG: %d %b %Y, %H:%M:%S")))
                 sys.exit()
             else:
-                print "Sorry, this is not a valid answer."
+                if textual:
+                    print "Sorry, this is not a valid answer."
                 logging.info("{}".format(time.strftime("END LOG: %d %b %Y, %H:%M:%S")))
                 sys.exit()
-        self.player = Player(profile.load(name))
         try:
             self.player = Player(profile.load(name))
         except:
-            print 'Your profile cannot be loaded.'
+            if textual:
+                print 'Your profile cannot be loaded.'
             logging.error(('Your profile cannot be loaded. This happens when the player '
                            'class has been modified in such a way that your profile no '
                            'longer has the necessary requirements to be loaded. '
                            'Delete it and start over. We are sorry for the inconvenience.'))
             sys.exit()
         logging.info("Loaded player profile.")
-        print "You are now ready to go!\n"
+        if textual:
+            print "You are now ready to go!\n"
         # self.player.info() # used only while testing
         # self.player.weapon.info() # same as above
         # self.player.armor.info() # same as above
@@ -63,13 +84,15 @@ class Game(object):
     def run(self):
         logging.info("Running.")
         while True:
-            opt = self.show_main_menu()
-            a = raw_input("Please choose one: ")
-            print '\n',
+            if textual:
+                opt = self.show_main_menu()
+                a = raw_input("Please choose one: ")
+                print '\n',
             if a in opt.keys():
                 exec opt[a]
             else:
-                print "Command is not valid.\n"
+                if textual:
+                    print "Command is not valid.\n"
 
     def show_main_menu(self):
         opt = {"i": "self.player.info();time.sleep(info_sleep)",
@@ -93,10 +116,11 @@ if __name__ == '__main__':
     root = os.getcwd()
     # sys.path.append(root)
     sys.path.append(os.path.join(root, 'core'))
-
-    check = raw_input('Check for updates?[y,n][default:n]').lower() or 'n'
+    if textual:
+        check = raw_input('Check for updates?[y,n][default:n]').lower() or 'n'
     if check == 'y':
-        print '\n',
+        if textual:
+            print '\n',
         update.update()
 
     game = Game()
